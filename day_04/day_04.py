8d854dbf445ce389240a78116ab75016a071380f
Pallav Dubey <pallav.dubey@thoughtworks.com>
2019-12-04 18:38:19 +0530

    day 4 complete

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -1,4 +1,4 @@
-def has_two_adj_numbers(number):
+def has_adj_numbers(number):
     string = str(number)
 
     i = 0
@@ -8,3 +8,58 @@ def has_two_adj_numbers(number):
         i += 1
 
     return False
+
+
+def is_decreasing(number):
+    string = str(number)
+
+    i = 0
+    while i < len(string) - 1:
+        if int(string[i]) > int(string[i + 1]):
+            return True
+        i += 1
+
+    return False
+
+
+def get_each_no_count(number):
+    string = str(number)
+
+    count_dict = {}
+
+    for ch in string:
+        if ch in count_dict:
+            count_dict[ch] += 1
+        else:
+            count_dict[ch] = 1
+
+    return count_dict
+
+
+def second_filter(number):
+    ch_count = get_each_no_count(number)
+
+    for c in ch_count:
+        if ch_count[c] == 2:
+            return True
+
+
+def main():
+    given_range = range(382345, 843167)
+
+    first_filtered_values = []
+    for i in given_range:
+        if has_adj_numbers(i) and not is_decreasing(i):
+            first_filtered_values.append(i)
+
+    print(f'count of filter one values: {len(first_filtered_values)}')
+
+    second_filtered_values = []
+    for i in first_filtered_values:
+        if second_filter(i):
+            second_filtered_values.append(i)
+
+    print(f'count of filter two values: {len(second_filtered_values)}')
+
+
+main()
--- day_04/day_04_test.py
+++ day_04/day_04_test.py
@@ -4,7 +4,17 @@ import day_04
 
 class Test(unittest.TestCase):
     def test_has_two_adj_numbers(self):
-        self.assertTrue(day_04.has_two_adj_numbers(12635633))
-        self.assertFalse(day_04.has_two_adj_numbers(123234))
-        self.assertTrue(day_04.has_two_adj_numbers(19346188))
-        self.assertFalse(day_04.has_two_adj_numbers(123234))
+        self.assertTrue(day_04.has_adj_numbers(22))
+        self.assertFalse(day_04.has_adj_numbers(123234))
+        self.assertTrue(day_04.has_adj_numbers(122345))
+        self.assertFalse(day_04.has_adj_numbers(123234))
+
+    def test_is_decreasing(self):
+        self.assertFalse(day_04.is_decreasing(111123))
+        self.assertFalse(day_04.is_decreasing(135679))
+        self.assertTrue(day_04.is_decreasing(5356))
+        self.assertTrue(day_04.is_decreasing(96326))
+
+
+if __name__ == '__main__':
+    unittest.main()
