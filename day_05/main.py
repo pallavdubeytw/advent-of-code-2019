c25e0d885a2c6e505e97ab1b5e4bcd1b25dfc477
Pallav Dubey <pallav.dubey@thoughtworks.com>
2019-12-05 22:03:45 +0530

    refactor | rename variables and remove unused methods

--- day_05/main.py
+++ day_05/main.py
@@ -1,39 +1,40 @@
-prog = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 65, 39, 225, 2, 14, 169, 224, 101, -2340, 224, 224, 4,
-        224, 1002, 223, 8, 223, 101, 7, 224, 224, 1, 224, 223, 223, 1001, 144, 70, 224, 101, -96, 224, 224, 4, 224,
-        1002, 223, 8, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1101, 92, 65, 225, 1102, 42, 8, 225, 1002, 61, 84, 224,
-        101, -7728, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 5, 224, 1, 223, 224, 223, 1102, 67, 73, 224, 1001,
-        224, -4891, 224, 4, 224, 102, 8, 223, 223, 101, 4, 224, 224, 1, 224, 223, 223, 1102, 54, 12, 225, 102, 67, 114,
-        224, 101, -804, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 224, 223, 223, 1101, 19, 79, 225,
-        1101, 62, 26, 225, 101, 57, 139, 224, 1001, 224, -76, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 2, 224, 1, 224,
-        223, 223, 1102, 60, 47, 225, 1101, 20, 62, 225, 1101, 47, 44, 224, 1001, 224, -91, 224, 4, 224, 1002, 223, 8,
-        223, 101, 2, 224, 224, 1, 224, 223, 223, 1, 66, 174, 224, 101, -70, 224, 224, 4, 224, 102, 8, 223, 223, 1001,
-        224, 6, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105,
-        227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105,
-        1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101,
-        294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0,
-        0, 1105, 1, 99999, 108, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 329, 101, 1, 223, 223, 1107, 226, 677, 224,
-        1002, 223, 2, 223, 1005, 224, 344, 101, 1, 223, 223, 8, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 359, 101, 1,
-        223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 374, 1001, 223, 1, 223, 1108, 226, 677, 224, 1002,
-        223, 2, 223, 1005, 224, 389, 101, 1, 223, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 404, 1001,
-        223, 1, 223, 1108, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 419, 1001, 223, 1, 223, 1008, 226, 677, 224, 102,
-        2, 223, 223, 1005, 224, 434, 101, 1, 223, 223, 107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 449, 1001, 223,
-        1, 223, 1007, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 464, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223,
-        223, 1005, 224, 479, 101, 1, 223, 223, 1007, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 494, 101, 1, 223, 223,
-        7, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 509, 101, 1, 223, 223, 1008, 677, 677, 224, 1002, 223, 2, 223,
-        1006, 224, 524, 1001, 223, 1, 223, 108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 539, 101, 1, 223, 223, 8,
-        226, 226, 224, 102, 2, 223, 223, 1006, 224, 554, 101, 1, 223, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1005,
-        224, 569, 1001, 223, 1, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 584, 101, 1, 223, 223, 1107,
-        677, 226, 224, 1002, 223, 2, 223, 1005, 224, 599, 101, 1, 223, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1006,
-        224, 614, 1001, 223, 1, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 629, 1001, 223, 1, 223, 107, 677,
-        226, 224, 1002, 223, 2, 223, 1005, 224, 644, 1001, 223, 1, 223, 1107, 677, 677, 224, 102, 2, 223, 223, 1006,
-        224, 659, 101, 1, 223, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 674, 1001, 223, 1, 223, 4, 223,
-        99, 226]
+int_code = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 65, 39, 225, 2, 14, 169, 224, 101, -2340, 224, 224,
+            4, 224, 1002, 223, 8, 223, 101, 7, 224, 224, 1, 224, 223, 223, 1001, 144, 70, 224, 101, -96, 224, 224, 4,
+            224, 1002, 223, 8, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1101, 92, 65, 225, 1102, 42, 8, 225, 1002, 61,
+            84, 224, 101, -7728, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 5, 224, 1, 223, 224, 223, 1102, 67, 73,
+            224, 1001, 224, -4891, 224, 4, 224, 102, 8, 223, 223, 101, 4, 224, 224, 1, 224, 223, 223, 1102, 54, 12, 225,
+            102, 67, 114, 224, 101, -804, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 224, 223, 223, 1101,
+            19, 79, 225, 1101, 62, 26, 225, 101, 57, 139, 224, 1001, 224, -76, 224, 4, 224, 1002, 223, 8, 223, 1001,
+            224, 2, 224, 1, 224, 223, 223, 1102, 60, 47, 225, 1101, 20, 62, 225, 1101, 47, 44, 224, 1001, 224, -91, 224,
+            4, 224, 1002, 223, 8, 223, 101, 2, 224, 224, 1, 224, 223, 223, 1, 66, 174, 224, 101, -70, 224, 224, 4, 224,
+            102, 8, 223, 223, 1001, 224, 6, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106,
+            227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280,
+            1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999,
+            1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 108, 226, 226, 224, 102, 2, 223, 223, 1005,
+            224, 329, 101, 1, 223, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 344, 101, 1, 223, 223, 8,
+            226, 677, 224, 102, 2, 223, 223, 1006, 224, 359, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223,
+            1005, 224, 374, 1001, 223, 1, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 389, 101, 1, 223, 223,
+            1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 404, 1001, 223, 1, 223, 1108, 677, 677, 224, 102, 2, 223,
+            223, 1006, 224, 419, 1001, 223, 1, 223, 1008, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 434, 101, 1, 223,
+            223, 107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 449, 1001, 223, 1, 223, 1007, 226, 677, 224, 102, 2,
+            223, 223, 1005, 224, 464, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 479, 101, 1, 223,
+            223, 1007, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 494, 101, 1, 223, 223, 7, 677, 677, 224, 102, 2, 223,
+            223, 1006, 224, 509, 101, 1, 223, 223, 1008, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 524, 1001, 223, 1,
+            223, 108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 539, 101, 1, 223, 223, 8, 226, 226, 224, 102, 2, 223,
+            223, 1006, 224, 554, 101, 1, 223, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 569, 1001, 223, 1,
+            223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 584, 101, 1, 223, 223, 1107, 677, 226, 224, 1002,
+            223, 2, 223, 1005, 224, 599, 101, 1, 223, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 614, 1001,
+            223, 1, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 629, 1001, 223, 1, 223, 107, 677, 226, 224,
+            1002, 223, 2, 223, 1005, 224, 644, 1001, 223, 1, 223, 1107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 659,
+            101, 1, 223, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 674, 1001, 223, 1, 223, 4, 223, 99,
+            226]
 
 
-# prog = [1002, 4, 3, 4, 33]
+# int_code = [1002, 4, 3, 4, 33]
 
 
-class Mode:
+class Operation:
     ADD = 1
     MULTIPLY = 2
     INPUT = 3
@@ -50,155 +51,145 @@ class ParameterMode:
     IMMEDIATE = 1
 
 
-def get_total_digits_count(number):
-    return len(str(number))
-
-
-def get_nth_digit(number, n):
-    if len(str(number)) < n:
+def get_nth_digit(instruction, n):
+    if len(str(instruction)) < n:
         return None
-    return int(str(number)[0 - n])
+    return int(str(instruction)[0 - n])
 
 
-def get_op_code(number):
-    return int(str(number)[-2:])
+def get_op_code(instruction):
+    return int(str(instruction)[-2:])
 
 
-def get_mode_first(number):
-    mode = get_nth_digit(number, 3)
+def get_first_param_mode(instruction):
+    mode = get_nth_digit(instruction, 3)
     if mode is None or mode is 0:
         return ParameterMode.POSITION
     return ParameterMode.IMMEDIATE
 
 
-def get_mode_second(number):
-    mode = get_nth_digit(number, 4)
+def get_second_param_mode(instruction):
+    mode = get_nth_digit(instruction, 4)
     if mode is None or mode is 0:
         return ParameterMode.POSITION
     return ParameterMode.IMMEDIATE
 
 
-def get_mode_third(number):
-    mode = get_nth_digit(number, 5)
-    if mode is None or mode is 0:
-        return ParameterMode.POSITION
-    return ParameterMode.IMMEDIATE
-
-
-def get_next_instruction_position_offset(mode):
-    if mode is Mode.ADD or mode is Mode.MULTIPLY or mode is Mode.LESS_THAN or mode is Mode.EQUALS:
+def get_next_instruction_position_offset(operation):
+    if operation is Operation.ADD or operation is Operation.MULTIPLY \
+            or operation is Operation.LESS_THAN or operation is Operation.EQUALS:
         return 4
-    elif mode is Mode.INPUT or mode is Mode.OUTPUT:
+    elif operation is Operation.INPUT or operation is Operation.OUTPUT:
         return 2
-    elif mode is Mode.JUMP_IF_TRUE or mode is Mode.JUMP_IF_FALSE:
+    elif operation is Operation.JUMP_IF_TRUE or operation is Operation.JUMP_IF_FALSE:
         return 3
 
 
-def add(ip, first, second, third, first_mode, second_mode):
-    op_1 = first if first_mode == ParameterMode.IMMEDIATE else ip[first]
-    op_2 = second if second_mode == ParameterMode.IMMEDIATE else ip[second]
+def op_add(program, param_1, first_param_mode, param_2, second_param_mode, param_3):
+    op_1 = param_1 if first_param_mode == ParameterMode.IMMEDIATE else program[param_1]
+    op_2 = param_2 if second_param_mode == ParameterMode.IMMEDIATE else program[param_2]
 
-    ip[third] = op_1 + op_2
-
-
-def multiply(ip, first, second, third, first_mode, second_mode):
-    op_1 = first if first_mode == ParameterMode.IMMEDIATE else ip[first]
-    op_2 = second if second_mode == ParameterMode.IMMEDIATE else ip[second]
-
-    ip[third] = op_1 * op_2
+    program[param_3] = op_1 + op_2
 
 
-def ip_mode(ip, first):
-    ip[first] = int(input('Enter: '))
+def op_multiply(program, param_1, first_param_mode, param_2, second_param_mode, param_3):
+    op_1 = param_1 if first_param_mode == ParameterMode.IMMEDIATE else program[param_1]
+    op_2 = param_2 if second_param_mode == ParameterMode.IMMEDIATE else program[param_2]
+
+    program[param_3] = op_1 * op_2
 
 
-def op_mode(ip, first):
-    print(ip[first])
+def op_input(program, param_1):
+    program[param_1] = int(input('Enter: '))
 
 
-def get_jump_if_true_index(ip, first, first_mode, second, second_mode):
-    op_1 = first if first_mode == ParameterMode.IMMEDIATE else ip[first]
-    op_2 = second if second_mode == ParameterMode.IMMEDIATE else ip[second]
+def op_output(program, param_1):
+    print(program[param_1])
+
+
+def op_jump_if_true(program, param_1, first_param_mode, param_2, second_param_mode):
+    op_1 = param_1 if first_param_mode == ParameterMode.IMMEDIATE else program[param_1]
+    op_2 = param_2 if second_param_mode == ParameterMode.IMMEDIATE else program[param_2]
 
     if op_1 != 0:
         return op_2
 
 
-def get_jump_if_false_index(ip, first, first_mode, second, second_mode):
-    op_1 = first if first_mode == ParameterMode.IMMEDIATE else ip[first]
-    op_2 = second if second_mode == ParameterMode.IMMEDIATE else ip[second]
+def op_jump_if_false(program, param_1, first_param_mode, param_2, second_param_mode):
+    op_1 = param_1 if first_param_mode == ParameterMode.IMMEDIATE else program[param_1]
+    op_2 = param_2 if second_param_mode == ParameterMode.IMMEDIATE else program[param_2]
 
     if op_1 == 0:
         return op_2
 
 
-def less_than(ip, first, second, third, first_mode, second_mode):
-    op_1 = first if first_mode == ParameterMode.IMMEDIATE else ip[first]
-    op_2 = second if second_mode == ParameterMode.IMMEDIATE else ip[second]
+def op_less_than(program, param_1, first_param_mode, param_2, second_param_mode, param_3):
+    op_1 = param_1 if first_param_mode == ParameterMode.IMMEDIATE else program[param_1]
+    op_2 = param_2 if second_param_mode == ParameterMode.IMMEDIATE else program[param_2]
 
     if op_1 < op_2:
-        ip[third] = 1
+        program[param_3] = 1
     else:
-        ip[third] = 0
+        program[param_3] = 0
 
 
-def equals(ip, first, second, third, first_mode, second_mode):
-    op_1 = first if first_mode == ParameterMode.IMMEDIATE else ip[first]
-    op_2 = second if second_mode == ParameterMode.IMMEDIATE else ip[second]
+def op_equals(program, param_1, first_param_mode, param_2, second_param_mode, param_3):
+    op_1 = param_1 if first_param_mode == ParameterMode.IMMEDIATE else program[param_1]
+    op_2 = param_2 if second_param_mode == ParameterMode.IMMEDIATE else program[param_2]
 
     if op_1 == op_2:
-        ip[third] = 1
+        program[param_3] = 1
     else:
-        ip[third] = 0
+        program[param_3] = 0
 
 
-def main_prog():
+def main_method(program):
     i = 0
-    while i < len(prog) - 3:
-        inst = prog[i]
+    while i < len(program) - 3:
+        instruction = program[i]
 
-        mode = get_op_code(inst)
+        op_code = get_op_code(instruction)
 
-        first = prog[i + 1]
-        second = prog[i + 2]
-        third = prog[i + 3]
-        mode_first = get_mode_first(inst)
-        mode_second = get_mode_second(inst)
+        param_1 = program[i + 1]
+        param_2 = program[i + 2]
+        param_3 = program[i + 3]
+        first_param_mode = get_first_param_mode(instruction)
+        second_param_mode = get_second_param_mode(instruction)
 
-        if mode is Mode.ADD:
-            add(prog, first, second, third, mode_first, mode_second)
+        if op_code is Operation.ADD:
+            op_add(program, param_1, first_param_mode, param_2, second_param_mode, param_3)
 
-        elif mode is Mode.MULTIPLY:
-            multiply(prog, first, second, third, mode_first, mode_second)
+        elif op_code is Operation.MULTIPLY:
+            op_multiply(program, param_1, first_param_mode, param_2, second_param_mode, param_3)
 
-        elif mode is Mode.INPUT:
-            ip_mode(prog, first)
+        elif op_code is Operation.INPUT:
+            op_input(program, param_1)
 
-        elif mode is Mode.OUTPUT:
-            op_mode(prog, first)
+        elif op_code is Operation.OUTPUT:
+            op_output(program, param_1)
 
-        elif mode is Mode.JUMP_IF_TRUE:
-            index = get_jump_if_true_index(prog, first, mode_first, second, mode_second)
+        elif op_code is Operation.JUMP_IF_TRUE:
+            index = op_jump_if_true(program, param_1, first_param_mode, param_2, second_param_mode)
             if index is not None:
                 i = index
                 continue
 
-        elif mode is Mode.JUMP_IF_FALSE:
-            index = get_jump_if_false_index(prog, first, mode_first, second, mode_second)
+        elif op_code is Operation.JUMP_IF_FALSE:
+            index = op_jump_if_false(program, param_1, first_param_mode, param_2, second_param_mode)
             if index is not None:
                 i = index
                 continue
 
-        elif mode is Mode.LESS_THAN:
-            less_than(prog, first, second, third, mode_first, mode_second)
+        elif op_code is Operation.LESS_THAN:
+            op_less_than(program, param_1, first_param_mode, param_2, second_param_mode, param_3)
 
-        elif mode is Mode.EQUALS:
-            equals(prog, first, second, third, mode_first, mode_second)
+        elif op_code is Operation.EQUALS:
+            op_equals(program, param_1, first_param_mode, param_2, second_param_mode, param_3)
 
-        else:
+        elif op_code is Operation.STOP:
             break
 
-        i += get_next_instruction_position_offset(mode)
+        i += get_next_instruction_position_offset(op_code)
 
 
-main_prog()
+main_method(int_code)
