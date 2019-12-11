cf3042f23ddc79d58031fb4df4bc5d5cda2667d0
Pallav Dubey <pallav.dubey@thoughtworks.com>
2019-12-11 17:50:07 +0530

    day 10 part 2 complete | needs refactoring

--- day_10/main.py
+++ day_10/main.py
@@ -88,4 +88,72 @@ for c in dc:
 
 print(f'ans: {ans} | {dc[ans]}')
 
-sp = Point(11, 13)
+p = list(map(int, ans.split(',')))
+sp = Point(p[0], p[1])
+# sp = Point(2, 2)
+dc2 = {}
+for y in range(0, len(pz_inp)):
+    for x in range(0, len(pz_inp[y])):
+        op = Point(x, y)
+        if str(op) == str(sp):
+            continue
+        if pz_inp[y][x] == '#':
+            angle = sp.angle(op)
+            dist = sp.distance(op)
+            if angle in dc2:
+                if dist < dc2[angle][1]:
+                    dc2[angle] = [op, dist]
+            else:
+                dc2[angle] = [op, dist]
+        x += 1
+    y += 1
+
+tot = len(dc2)
+
+l1 = []
+l4 = []
+l3 = []
+l2 = []
+for a in dc2:
+    if a >= 270 and a < 360:
+        l1.append(a)
+    elif a >= 0 and a < 90:
+        l2.append(a)
+    elif a >= 90 and a < 180:
+        l3.append(a)
+    elif a >= 180 and a < 270:
+        l4.append(a)
+    else:
+        print(a)
+
+tot = len(l1) + len(l2) + len(l3) + len(l4)
+
+l1.sort()
+l4.sort()
+l3.sort()
+l2.sort()
+
+tot = len(l1) + len(l2) + len(l3) + len(l4)
+
+print(dc2)
+# print(l1)
+# print(l2)
+# print(l3)
+# print(l4)
+
+i = 0
+for a in l1:
+    i += 1
+    print(f'{i} : {a} : {dc2[a][0]}')
+for a in l2:
+    i += 1
+    print(f'{i} : {a} : {dc2[a][0]}')
+for a in l3:
+    i += 1
+    print(f'{i} : {a} : {dc2[a][0]}')
+for a in l4:
+    i += 1
+    if a == 360:
+        print(f'{i} : {a} : {dc2[0][0]}')
+    else:
+        print(f'{i} : {a} : {dc2[a][0]}')
