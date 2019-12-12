a255e76c791debcef0f69fa09a808c78f486284c
Pallav Dubey <pallav.dubey@thoughtworks.com>
2019-12-13 01:35:05 +0530

    add print progress util

---
+++ utils/progress.py
@@ -0,0 +1,5 @@
+def print_progress(total, completed, new_line=False):
+    if not new_line:
+        print(f'progress: {int((completed / total) * 100)}', end='\r')
+    else:
+        print(f'progress: {int((completed / total) * 100)}')
