38e0f0613ed48351f2a5bb7f62a12737c7c855eb
Pallav Dubey <pallav.dubey@thoughtworks.com>
2019-12-21 18:52:37 +0530

    part 2 complete

--- day_16/main_2.py
+++ day_16/main_2.py
@@ -4,28 +4,18 @@ from utils.progress import print_progress
 
 file = open('input.txt', 'r')
 inp_str = file.readline()
-inp_str = '03081770884921959731165446850517'
 
 pz_input = []
 for i in inp_str:
     pz_input.append(int(i))
 
 
-# @calc_run_time
-def get_element(signal, element):
-    # cycle = int(len(signal) / element)
-    total = sum(signal[element:])
-    return total % 10
-
-
-# @calc_run_time
 def get_phase_output(signal, offset):
-    length = len(signal) - offset
-    phase_output = [0] * length
-    index = length - 1
-    for i in range(len(signal) - 1, offset - 1, -1):
-        phase_output[index] = get_element(signal, i)
-        index -= 1
+    phase_output = []
+    t = sum(signal[offset:])
+    for i in range(offset, len(signal)):
+        phase_output.append(t % 10)
+        t -= signal[i]
 
     return phase_output
 
@@ -37,7 +27,7 @@ def part_two(ip):
     empty = [0] * offset
     outp = []
     for i in range(0, 100):
-        print(f'phase: {i}')
+        print_progress(100, i)
         outp = get_phase_output(signal, offset)
         signal = empty + outp
 
